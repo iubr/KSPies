a16c5cac6ead429476581a0a5754710608a4852e
iubr <iubr@umk.pl>
2025-01-21 17:05:19 +0100

    modified setup for newer python and numpy versions, since numpy.distutils is no longer used

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-import setuptools
-from numpy.distutils.core import setup, Extension
+import os
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
-print(setuptools.find_packages())
+# From:
+# https://stackoverflow.com/questions/64950460/link-f2py-generated-so-file-in-a-python-package-using-setuptools
+class f2py_Extension(Extension):
 
-lib = Extension(
-        name='kspies.kspies_fort',
-        sources=['kspies/kspies_fort.f90'],
-        library_dirs=['/usr/lib/'],
-        include_dirs=['/usr/include'],
-        libraries=['lapack']
-        )
+    def __init__(self, name, sourcedirs):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedirs = [os.path.abspath(sourcedir) for sourcedir in sourcedirs]
+        self.dirs = sourcedirs
+
+class f2py_Build(build_ext):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        # compile
+        for ind, to_compile in enumerate(ext.sourcedirs):
+            module_loc = os.path.split(ext.dirs[ind])[0]
+            module_name = os.path.split(to_compile)[1].split('.')[0]
+            os.system('cd %s;f2py -c %s -m %s' % (module_loc, to_compile, module_name))
 
 data_files = [
     ('fortran_source', ['kspies/kspies_fort.f90', 'kspies/compile.sh'])
@@ -27,9 +40,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://ssnam92.github.io/KSPies/",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     data_files=data_files,
-    ext_modules = [lib],
+    ext_modules = [f2py_Extension('fortran_external', ['kspies/kspies_fort.f90'])],
+    cmdclass=dict(build_ext=f2py_Build),
     install_requires=[
       "numpy>=1.18.4",
       "scipy>=1.4.1",
